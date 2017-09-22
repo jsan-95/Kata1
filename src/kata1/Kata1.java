873c8fe160ddef828fd510c6f897325b97c7a2b1
Yisus95 <Yisus95@10.230.172.158>
2017-09-22 09:19:46 +0100

    Kata1: cálculo de la edad de una persona usando la clase obsoleta Date

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Kata1 {
     
     public static void main(String[] args) {
-        Person jesus = new Person("Jesus",new Date(01,05,16));
+        Person jesus = new Person("Jesus",new Date(01,01,10));
         System.out.println("Me llamo "+jesus.getName()+" y tengo "+jesus.getAge()+" años");        
     }
 }
